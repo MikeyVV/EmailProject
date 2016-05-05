7ac64faa47a806c609aee9ddd43bcf6ffa65500d
MikeVV <w.nanthakon@gmail.com>
2016-05-05 18:28:34 +0700

    Edit3

--- loveServer/loveServer/MainWindow.xaml.cs
+++ loveServer/loveServer/MainWindow.xaml.cs
@@ -1,39 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Windows;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace loveServer
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
-
-    
-    
-
     public partial class MainWindow : Window
     {
-
+        public static void ThreadMethod()
+        {
+            
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
-
     }
 }
