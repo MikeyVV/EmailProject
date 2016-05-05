4406ab84ec380fea1c7a9e1d288942c30a063a38
Zamasu <chinnawat.sinnarai@gmail.com>
2016-05-05 19:00:51 +0700

    WinTheard

---
+++ WinTheard/WinTheard/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace WinTheard
+{
+    public partial class Form1 : Form
+    {
+        delegate void StringParameterDelegate(int count);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartThread_Click(object sender, EventArgs e)
+        {
+            Thread workThread = new Thread(new ThreadStart(Counting));
+            workThread.IsBackground = true;
+            workThread.Start();
+        }
+
+        private void Counting()
+        {
+            for (int i = 0; i < 10000000; i++)
+            {
+                UpdateLabelCount(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        private void UpdateLabelCount(int count)
+        {
+            if (InvokeRequired)
+            {
+                // We're not in the UI thread, so we need to call BeginInvoke
+                BeginInvoke(new StringParameterDelegate(UpdateLabelCount), new object[] { count });
+                return;
+            }
+            label1.Text = count.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
